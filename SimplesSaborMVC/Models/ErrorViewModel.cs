43293b3ecb332bae0e48c5843eae0fd1c72e847c
heloez <escolaanaehelo@gmail.com>
2024-08-09 12:26:14 -0300

    Criado o projeto MVC e correção da estrutura de pastas

---
+++ SimplesSaborMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SimplesSaborMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
